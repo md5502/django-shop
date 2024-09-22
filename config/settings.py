25a7dd4b79043271cb7d6b4706ff7518c01d4921
md5502 <m.baniasadi.d@gmail.com>
2024-09-22 18:52:54 +0330

    config the jwt

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     # local
     "users.apps.UsersConfig",
     "common.apps.CommonConfig",
+
+    # 3rd party
+    "rest_framework_simplejwt",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -132,3 +136,11 @@ AUTH_USER_MODEL = "users.BaseUser"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
